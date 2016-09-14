236863df8074d892d1a1f14d2e0a95011221f48b
gonghao <gonghao114@sina.com>
2016-09-14 18:37:08 +0800

    文章页修改

--- client/src/components/admin/article.js
+++ client/src/components/admin/article.js
@@ -1,8 +1,15 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Table } from 'antd';
 
 class Article extends Component {
   render() {
+    const columns = [{
+      title: '标题',
+      dataIndex: 'title',
+      key: 'title'
+    }];
+
     return (
       <div>MM</div>
     );
